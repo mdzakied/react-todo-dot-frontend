b0057dd03cf27c3aed42eba78b69d7636c2ee752
mdzakied <mdzakied123@gmail.com>
2025-07-24 21:31:31 +0700

    fix vercel route

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const __dirname = dirname(__filename)
 
 export default defineConfig({
   plugins: [react()],
-  base: '/login',
+  base: '/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
